354c45bdc1f6d79c659b48f9295658d21523fbd8
Lia Forgione <liaforg@gmail.com>
2017-07-17 15:38:25 -0400

    added login view with form in sessions views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   get '/users/new' => 'users#new'
-  # get '/'
+  get '/sessions/new' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
