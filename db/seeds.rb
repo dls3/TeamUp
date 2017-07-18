f3148fa2c737c997e86e98271c2dfa5b3a0ca270
Doug <doug.steinberg@gmail.com>
2017-07-18 17:17:46 -0400

    If statement to only import public courts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,18 @@
 # ADDING TENNIS COURTS TO COURT_FIELDS FROM CSV
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'tennis_courts_facilities_data_2017.csv'))
-csv = CSV.parse(csv_text, :headers => true) #, :encoding => 'ISO-8859-1')
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Tennis Courts Facilities Data 2017.csv'))
+csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|  # headers become keys
-  t = CourtField.new
-  t.name = row['NAME']
-  t.address = row['ADDRESS']
-  t.neighbourhood = row['NEIGHBOURHOOD']
-  t.save
-  puts "#{t.name} saved"
+  if row['Primary Permitting Status'] == 'Public'
+    court = CourtField.new
+
+    court.name = row['NAME']
+    court.address = row['ADDRESS']
+    court.neighbourhood = row['DISTRICT']
+    court.save
+    puts "#{court.name} was saved"
+  end
 end
 
-puts "There are #{CourtField.count} rows in the court_fields table"
+# puts "There are #{CourtField.count} courts saved in the court_fields table"
