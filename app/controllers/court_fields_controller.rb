f22596ce3ce9b0c059d8f1d35abb1217f36c6166
Doug <doug.steinberg@gmail.com>
2017-07-18 15:18:16 -0400

    Attempt to load tennis court  csv in court fields controller

--- app/controllers/court_fields_controller.rb
+++ app/controllers/court_fields_controller.rb
@@ -1,2 +1,8 @@
 class CourtFieldsController < ApplicationController
+  require 'csv'
+
+  CSV.foreach('tennis_courts_facilities_data_2017.csv', :headers => true) do |row|
+    Moulding.create!(row.to_hash)
+  end
+
 end
