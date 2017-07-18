3e853cc37464b83bf4c92001949aadcf4eadc25a
Doug <doug.steinberg@gmail.com>
2017-07-18 16:33:02 -0400

    Simplify csv to match current schema

--- app/models/court_field.rb
+++ app/models/court_field.rb
@@ -8,22 +8,16 @@ class CourtField < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|    # line by line
 
-    tennis_courts_hash = row.to_hash # exclude the price field
-    tennis_courts = CourtField.where(id: tennis_courts_hash["id"])
+      tennis_courts_hash = row.to_hash # exclude the price field
+      tennis_courts = CourtField.where(id: tennis_courts_hash["id"])
+      # puts tennis_courts
 
-    CourtField.create!(tennis_courts_hash)
+      CourtField.create!(tennis_courts_hash)
     end
   end
 
 end
 
 
-# at once
+
 # tennis_courts = CSV.read('tennis_courts_facilities_data_2017.csv)'
-
-# line by line
-
-
-# CSV.foreach('tennis_courts_facilities_data_2017.csv', :headers => true) do |row|
-#   Moulding.create!(row.to_hash)
-# end
