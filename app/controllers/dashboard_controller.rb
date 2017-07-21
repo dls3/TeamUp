9b41f4ad433c196bf00b8b92b44cf2749dc023ac
Doug <doug.steinberg@gmail.com>
2017-07-21 17:07:19 -0400

    Remove duplicate formats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,28 +1,13 @@
 class DashboardController < ApplicationController
 
-  def index
-    @courts = CourtField.all
-    @court = CourtField.new
+  def populate     # Ajax request to get json
+    # @courts = CourtField.all
+    #
+    # format.json do
+    #   if @court.save
+    #     render :json => @court
+    #   end
+    # end
   end
-
-  def create
-    @court = CourtField.new
-
-    if request.xhr?
-        respond_to do |format|
-          format.html do
-            if @court.save
-              render partial: 'court', locals: {court: @court}
-            end
-          end
-
-    format.json do
-          if @court.save
-            render :json => @court
-          end
-        end
-  end
-
-
 
 end
