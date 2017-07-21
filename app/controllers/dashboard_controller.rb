5e0384650e4d9f9c7fbac56cd5ef3a3c55db1e85
Doug <doug.steinberg@gmail.com>
2017-07-21 16:50:05 -0400

    create method in dashboard controller to get json format

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,28 @@
 class DashboardController < ApplicationController
 
   def index
-
+    @courts = CourtField.all
+    @court = CourtField.new
   end
+
+  def create
+    @court = CourtField.new
+
+    if request.xhr?
+        respond_to do |format|
+          format.html do
+            if @court.save
+              render partial: 'court', locals: {court: @court}
+            end
+          end
+
+    format.json do
+          if @court.save
+            render :json => @court
+          end
+        end
+  end
+
+
 
 end
--- app/models/court_field.rb
+++ app/models/court_field.rb
@@ -3,7 +3,4 @@ class CourtField < ApplicationRecord
   has_many :games
   validates :name, :address, :neighbourhood, presence: true
 
-# https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDpZWSriktrzfz64A9G6mcem3mYHuIJqpM
-
-
 end
