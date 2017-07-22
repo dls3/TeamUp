cfabd1f14ad328156c73cd8ffdfdd8fbdbb91ca5
Doug <doug.steinberg@gmail.com>
2017-07-22 17:17:50 -0400

    Animation to drop

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -67,7 +67,7 @@ function createMarkers(map) {
     var court = courts[i];
     var marker = new google.maps.Marker({
       position: {lat: court[1], lng: court[2]},
-      animation: google.maps.Animation.BOUNCE,
+      animation: google.maps.Animation.DROP,
       map: map,
       icon: image,
       shape: shape,
