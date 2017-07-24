3756addd13fadbd9e048f6edeedd125a2625a8b2
MASHA <mashanikit@gmail.com>
2017-07-24 12:49:55 -0400

    added search model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,14 @@
 class SearchController < ApplicationController
+  def index
+  end
+
+  def index
+    @games = Game.search(params[:game])
+  end
+end
+
+
+private
+def task_params
+  params.require(:game).permit(:sport, :neighbourhood)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721194630) do
+ActiveRecord::Schema.define(version: 20170724163617) do
 
   create_table "court_fields", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20170721194630) do
   create_table "rsvps", force: :cascade do |t|
     t.integer "user_id"
     t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
