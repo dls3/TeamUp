7c6d956506bd63592d5e91a3dc99688ae770840b
Doug <doug.steinberg@gmail.com>
2017-07-30 18:44:58 -0400

    Looping through courts fix

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,5 +1,6 @@
 function initMap() {
   console.log('inside init map');
+  console.log( document.getElementById('map'));
 
   var toronto = new google.maps.LatLng(43.642,-79.387);
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -8,73 +9,55 @@ function initMap() {
   });
 
   $.ajax({
-    url: 'http://localhost:3000/courts',
+    url: '/courts',
     method: 'GET',
-    dataType: 'json',
-    }).done(function(results) {
-    for (var i = 0; i < results.length; i++) {
-      var results = results[i];
-      console.log(' 1 2 1 2 1 2 1 2 1 2 1 2 1 2')
-      var marker = new google.maps.Marker({
-        position: {lat: results.lat, lng: results.long},
-        animation: google.maps.Animation.DROP,
-        map: map,
-        icon: image,
-        shape: shape,
-        title: results.name
-      });
-      var contentString = '<h3 id="firstHeading" class="firstHeading">Join this game!</h3>'+
-      '<div id="bodyContent">'+ '<p><b>Click here</b> to join this tennis match at <br>' + results.name + '</p></div>';
+    dataType: 'json'
+  }).done(function(results) {
+      // console.log(results["courts"].length);
 
-      AddInfowWindow(marker, contentString);
+      for (var i = 0; i < results["courts"].length; i++) {
+        console.log("INSIDE LOOP")
 
-      function AddInfowWindow(marker, contentString) {
-        var infoWindow = new google.maps.InfoWindow({
-          content: contentString
+        var results = JSON.parse(results)
+        console.log(results)
+        var marker = new google.maps.Marker({
+          position: {lat: results["courts"]["lat"], lng: results["courts"]["long"]},
+          animation: google.maps.Animation.DROP,
+          map: map,
+          icon: image,
+          shape: shape,
+          title: results["courts"]["name"]
         });
-        google.maps.event.addListener(marker, 'click', function() {
-          if (!marker.open) {
-              infoWindow.open(map,marker);
-              marker.open = true;
-          }
-          else {
-              infoWindow.close();
-              marker.open = false;
-          };
-        });
-      };
+        var contentString = '<h3 id="firstHeading" class="firstHeading">Join this game!</h3>'+
+        '<div id="bodyContent">'+ '<p><b>Click here</b> to join this tennis match at <br>' + results["courts"]["name"] + '</p></div>';
 
+        AddInfowWindow(marker, contentString);
 
+      }   // CLOSE LOOP
 
-
-    }
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-
-
-  });  // Close .done function
-
-  // Rails.root.join("assets", "lib", "tennis-15.svg")
-  var image = {
-    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    // This marker is 20 pixels wide by 32 pixels high.
-    size: new google.maps.Size(20, 32),
-    // The origin for this image is (0, 0).
-    origin: new google.maps.Point(0, 0),
-    // The anchor for this image is the base of the flagpole at (0, 32).
-    anchor: new google.maps.Point(0, 32)
-  };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-  var shape = {
-    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-    type: 'poly'
-  };
-
-}  // Close InitMap function
+    })  // *** CLOSES DONE FUNCTION ***
+    .done( function() {
+      console.log("AJAX REQUEST done")
+    }) // *** CLOSES DONE ***
+  };  // CLOSES MAIN FUNCTION ***
 
 closeInfoWindow = function() {
   infoWindow.close();
+};
+
+function AddInfowWindow(marker, contentString) {
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    if (!marker.open) {
+        infoWindow.open(map,marker);
+        marker.open = true;
+    }
+    else {
+        infoWindow.close();
+        marker.open = false;
+    };
+  });
 };
