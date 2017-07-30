0dbed500f8b1b05c0aeb74e3567ac11df8e4b646
Doug <doug.steinberg@gmail.com>
2017-07-30 19:39:16 -0400

    All courts showing on map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -8,6 +8,23 @@ function initMap() {
     zoom: 11
   });
 
+  var image = {
+    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
+  // Shapes define the clickable region of the icon. The type defines an HTML
+  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+  // The final coordinate closes the poly by connecting to the first coordinate.
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+
   $.ajax({
     url: '/courts',
     method: 'GET',
@@ -15,20 +32,29 @@ function initMap() {
   }).done(function(results) {
       // console.log(results["courts"].length);
 
-      for (var i = 0; i < results["courts"].length; i++) {
-        console.log("INSIDE LOOP")
-        results = results["courts"][i]
-        console.log(results)
+      for (var i = 0; i <= results["courts"].length; i++) {
+      // for (var i = 0; i <= 10; i++) {
+        // results = results["courts"][i]
+
+        var lat = Number(results["courts"][i]["lat"]);
+        var long = Number(results["courts"][i]["long"]);
+
+        console.log(lat, long);
+        console.log(typeof(lat));
+
+        // var myLatLng = {lat: Number(results["courts"][i]["lat"]), lng: Number(results["courts"][i]["long"]);
+
         var marker = new google.maps.Marker({
-          position: {lat: results["courts"]["lat"], lng: results["courts"]["long"]},
-          animation: google.maps.Animation.DROP,
           map: map,
+          // position: {lat: Number(results["courts"][i]["lat"]), lng: Number(results["courts"][i]["long"])},
+          position: {lat: long, lng: lat},
+          animation: google.maps.Animation.DROP,
           icon: image,
           shape: shape,
-          title: results["courts"]["name"]
+          title: results["courts"][i]["name"]
         });
         var contentString = '<h3 id="firstHeading" class="firstHeading">Join this game!</h3>'+
-        '<div id="bodyContent">'+ '<p><b>Click here</b> to join this tennis match at <br>' + results["courts"]["name"] + '</p></div>';
+        '<div id="bodyContent">'+ '<p><b>Click here</b> to join this tennis match at <br>' + results["courts"][i]["name"] + '</p></div>';
 
         AddInfowWindow(marker, contentString);
 
