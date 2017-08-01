9c39265bca8afeb1386d5ac014fc6aa482e3241c
MASHA <mashanikit@gmail.com>
2017-07-31 20:08:10 -0400

    fixed join page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,10 @@ class SearchesController < ApplicationController
   def search_results
     @user = User.find(session[:user_id])
     # @games = @user.games.where(sport:params[:sport])
-    @games = Game.where(sport:params[:sport])
-    @games = Game.where(neighbourhood:params[:neighbourhood])
     # @games.where(['sport' = ? OR 'neighbourhood' = ?, :sport, :neighbourhood])
     # Game.where(sport: '?')
 
-    @games = Game.where("sport ILIKE ?", "%#{params[:sport]}%")
+    @games = Game.where("sport ILIKE ?", "%#{params[:sport]}%") 
     @games = Game.where("neighbourhood ILIKE ?", "%#{params[:neighbourhood]}%")
     # filter thrugh the games code
 
